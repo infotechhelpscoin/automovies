f5539d54faf3f10f112bed538e19894014b72b10
enayetaitech <mdrehman@365aitech.com>
2024-05-28 10:20:02 +0600

    server coded added

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ const cloudinary = require('./config/cloudinaryConfig')
 const { connect } = require('./mongoConnection')
 const { getChatGPTAPI, setupChatGPTAPI} = require('./config/gptConfig')
 const cron = require('node-cron');
-// const gptPromptGenerate = require('./scheduleTask/promptGenerate');
-// const { generateMidjourneyImages } = require('./scheduleTask/mainMidjourney');
-// const { videoGenerationSchedule } = require('./scheduleTask/videoGenerate');
-// const { uploadToYoutube } = require('./scheduleTask/uploadToYoutube');
+
 
 
 
@@ -67,38 +64,13 @@ async function startServer() {
     await connect();
     console.log('Connected to MongoDB successfully.');
 
-    // Manually trigger all tasks once at server start
-    // await runScheduledTasks();
-
-    // Then set up the cron job to run subsequently every 30 minutes
-    // cron.schedule('*/10 * * * *', runScheduledTasks);
-
-    const PORT = process.env.PORT || 3000;
+     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
     process.exit(1);
-  }
-}
-
-async function runScheduledTasks() {
-  console.log("Scheduled tasks started at:", new Date());
-  try {
-    await gptPromptGenerate();
-    console.log("Completed GPT Prompt Generation");
-
-    await generateMidjourneyImages();
-    console.log("Completed Generating Midjourney Images");
-
-    await videoGenerationSchedule();
-    console.log("Completed Video Generation Schedule");
-
-    await uploadToYoutube();
-    console.log("Completed Uploading to YouTube");
-  } catch (taskError) {
-    console.error("Error during scheduled tasks:", taskError);
   }
 }
 
