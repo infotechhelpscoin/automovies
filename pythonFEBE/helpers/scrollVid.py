832f2f3f34121d04f123c2aaa4df6ff87a484dc6
infotechhelpscoin hhh <info@techhelps.co.in>
2024-06-17 12:12:07 +0000

    kjjhjkn

--- pythonFEBE/helpers/scrollVid.py
+++ pythonFEBE/helpers/scrollVid.py
@@ -4,6 +4,9 @@ import os
 import base64
 
 async def create_video_from_url(url, savepath):
+    screenshot_path = 'screenshot.png'
+    screenshot = 'http://res.cloudinary.com/dj3qabx11/image/upload/v1718625872/unvuiirkbw5lymasrwyg.png'
+    video_length=72.5
     if False:
         result = subprocess.run(
             ['node', 'helpers/recordBrowser.js', url, savepath],
@@ -16,9 +19,8 @@ async def create_video_from_url(url, savepath):
         else:
             print("Error during recording")
             print(result.stderr)
-    print(url)
-    if False:
-        screenshot = await get_screenshot(
+
+        await get_screenshot(
                 {'url': url,
                 'width': 1280,
                 'height': 1280,
@@ -27,51 +29,31 @@ async def create_video_from_url(url, savepath):
                 'response_type': 'json',
                 'delay_time': 2000,
                 'timeout': 60000})
-        screenshot_path = None
-        if screenshot is not None:
-            base64_image = screenshot['image'].split(',', maxsplit=1)[1]
-            screenshot_path = 'screenshot.png'
-            image_file = open(screenshot_path, 'wb')
-            image_file.write(base64.b64decode(base64_image))
-            image_file.close()
-    video_length = 0
-    if os.path.exists(savepath):
-        result = subprocess.run(
-            ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', savepath],
-            capture_output=True,
-            text=True
-        )
-        if result.returncode == 0:
-            video_length = float(result.stdout.strip())
-    return 'asdas'
+        
+        video_length = 0
+        if os.path.exists(savepath):
+            result = subprocess.run(
+                ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', savepath],
+                capture_output=True,
+                text=True
+            )
+            if result.returncode == 0:
+                video_length = float(result.stdout.strip())
+                print(video_length)
+    
     
     # Generate HTML response
     html_response = f"""
-    <html>
-    <head>
-        <title>Video Recording Result</title>
-    </head>
-    <body>
-        <h1>Video Recording Result</h1>
+               <h1>Video Recording Result</h1>
         <p>Video length: {video_length} seconds</p>
+
+    <div class="image-container">
+        <img src="{screenshot}" alt="Image Preview">
+    </div>
+
         """
     
-    if screenshot_path:
-        # Convert screenshot path to base64
-        with open(screenshot_path, 'rb') as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-        
-        html_response += f"""
-        <h2>Screenshot:</h2>
-        <img src="data:image/png;base64,{encoded_string}" alt="Screenshot" />
-        """
-    
-    html_response += """
-    </body>
-    </html>
-    """
-    
-    return html_response,video_length,screenshot_path,savepath
+    return html_response
 
 # Example usage
 # url = "https://github.com/CopilotKit/CopilotKit?utm_source=manuagi.beehiiv.com&utm_medium=referral&utm_campaign=top-trending-open-source-github-projects-this-week-decentralized-ai-agents-to-coding-assistants"
--- pythonFEBE/helpers/urltoscreenshot.py
+++ pythonFEBE/helpers/urltoscreenshot.py
@@ -1,18 +1,54 @@
 #!/usr/bin/python3
 import base64
 import requests
+import json
+import cloudinary
+import asyncio
+import cloudinary.uploader
+
+cloudinary.config(
+  cloud_name = 'dj3qabx11',  # Replace with your cloud name
+  api_key = '533762782692462',        # Replace with your API key
+  api_secret = 'YcvSAvEFsEu-rZyhKmLnI3bQ5KQ'   # Replace with your API secret
+)
+# cloudinary.config({ 
+#   cloud_name: 'dj3qabx11', 
+#   api_key: '533762782692462', 
+#   api_secret: 'YcvSAvEFsEu-rZyhKmLnI3bQ5KQ'
+# })
+
+# cloudinary.config({
+#   cloud_name: 'doyry0ttt',
+#   api_key: '447229656995129',
+#   api_secret: 'cZVTKK_zWBLpi3eeML6PcDKS2E4'
+# })
 
 
-async def get_screenshot(params):
+def upload_image(image_path):
+    """ Uploads an image to Cloudinary """
+    response = cloudinary.uploader.upload(image_path)
+    return response
+
+
+async def get_screenshot(params,screenshot_path):
     headers = {"Content-type": "application/x-www-form-urlencoded",
                "Accept": "text/plain",
                "userkey": "IAAIEYKBJAMIG76IQ3GROBJOXO"}
-
+    print('11')
     try:
         r = requests.post('https://api.site-shot.com/', headers=headers, data=params)
 
         if (r.status_code == requests.codes.ok):
-            return r.json()
+            screenshot=r.json()
+            if screenshot is not None:
+                base64_image = screenshot['image'].split(',', maxsplit=1)[1]
+                image_file = open(screenshot_path, 'wb')
+                image_file.write(base64.b64decode(base64_image))
+                image_file.close()
+            response = upload_image(screenshot_path)
+            url = response.get('url')
+            print("URL of the uploaded image:", url)            
+            return url
         elif (r.status_code == 404):
             print("Screenshot hasn't been generated. The error: " + r.json().error)
         elif (r.status_code == 401):
@@ -22,10 +58,12 @@ async def get_screenshot(params):
 
     except requests.exceptions.RequestException as e:
         print('Screenshot generation has failed, the error: ' + str(e))
+    
+            
 
 
-# def main():
-#     screenshot = get_screenshot(
+
+# asyncio.run( get_screenshot(
 #         {'url': 'https://github.com/OthersideAI/self-operating-computer/',
 #          'width': 1280,
 #          'height': 1280,
@@ -33,7 +71,7 @@ async def get_screenshot(params):
 #          'full_size':1,
 #          'response_type': 'json',
 #          'delay_time': 2000,
-#          'timeout': 60000})
+#          'timeout': 60000},'screenshot2.png'))
 
 #     if screenshot is not None:
 #         base64_image = screenshot['image'].split(',', maxsplit=1)[1]
